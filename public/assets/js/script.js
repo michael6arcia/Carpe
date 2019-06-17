842d022b4bd078d7f38ef039b3fa75d3ab4bbcd8
mchlgrca <mgarcia0898@gmail.com>
2019-06-16 20:34:00 -0700

    delete and complete buttons have functionality

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-    
-  $(".complete-form").on("submit", function(event) {
+$(document).ready(function () {
+
+  $(".complete-form").on("submit", function (event) {
     event.preventDefault();
 
     var goal_id = $(this).children(".goal_id").val();
@@ -8,10 +8,18 @@ $(document).ready(function() {
     $.ajax({
       method: "PUT",
       url: "/goals/" + goal_id
-    }).then(function(data) {
+    }).then(function (data) {
       // reload page to display completed goals in proper column
       location.reload();
     });
 
+  });
+
+  $(".delete-btn").on("click", function () {
+    alert("del-btn working");
+  });
+
+  $(".complete-btn").on("click", function () {
+    alert("comp-btn working");
   });
 });
